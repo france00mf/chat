cc3bf9c62283d7bb30b3da8cf5078867a3d518cd
FranceMF00 <franciscodomingosjose06798@gmail.com>
2020-05-07 19:31:07 +0100

    build: put icons item on menu

--- src/components/shared/menu/menu.vue
+++ src/components/shared/menu/menu.vue
@@ -1,8 +1,18 @@
 <template>
     <div class="container-menu">
-        <div class="item-menu"><user-avatar borderProps="4px solid black"></user-avatar></div>
-        <div class="item-menu">Domingos</div>
-        <div class="item-menu">José</div>
+        <div class="item-menu user"><user-avatar borderProps="4px solid black"></user-avatar></div>
+        <div class="item-menu icons">
+            
+              <a href="www.facebook.com"><i class="fa fa-home" aria-hidden="true"></i></a>
+               <a href="www.facebook.com"><i class="fa fa-paper-plane" aria-hidden="true"></i></a>
+              <a href="www.facebook.com"><i class="fa fa-inbox" aria-hidden="true"></i></a>
+              <a href="www.facebook.com"><i class="fa fa-users" aria-hidden="true"></i></a>
+              <a href="www.facebook.com"><i class="fa fa-bell" aria-hidden="true"></i></a>
+            <a href="www.facebook.com"><i class="fa fa-ellipsis-h" aria-hidden="true"></i></a>
+        </div>
+        <div class="item-menu config">
+            <a href="www.facebook.com"><i class="fa fa-cog" aria-hidden="true"></i></a>
+        </div>
     </div>
 </template>
 
@@ -15,9 +25,25 @@
     height: 100vh;
 }
 .item-menu{
-    flex: 1;
     width: 100%;
     color: white;
+}
+.item-menu a{
+    color: #555;
+    font-size: 2rem;
+}
+.icons{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+}
+.config{
+    display: flex;
+}
+.config a{
+    align-self: flex-end;
+    margin: 0 auto;
+    padding-bottom: 10px;
 }
 </style>
 
