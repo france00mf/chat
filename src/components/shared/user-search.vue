e0880302db9a55dc1b58b037a55cd735efff589a
FranceMF00 <franciscodomingosjose06798@gmail.com>
2020-05-21 12:25:59 +0100

    build: change user-search component

--- src/components/shared/user-search.vue
+++ src/components/shared/user-search.vue
@@ -1,8 +1,31 @@
 <template>
-            <div>
-                <div>
-            <i class="fab fa-facebook"></i>
+            <div class="user-search">
+            <i class="fa fa-search"></i>
             <input placeholder="Enter for Search"/>
             </div>
-            </div>
+        
 </template>
+
+<style scoped>
+.user-search{
+    display: flex;
+    background: #f9f8f8;
+    padding: 4px;
+    border-radius: 50px;
+}
+.user-search i{
+   padding: 10px;
+    color: red;
+    background: red;
+}
+.user-search input{
+    border: none;
+    background: transparent;
+}
+.user-search input::placeholder{
+    padding-left: 5px ;
+    /* color: red; */
+    color: #d3d2d5;
+}
+
+</style>
